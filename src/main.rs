cfc61fa55dcec416e868884ff6ac2992d3da4ee4
Waradu <waradu@outlook.com>
2024-07-24 10:43:52 +0200

    added transparency

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::WindowLevel};
 
 fn main() {
-    App::new().run();
+    let window = Window {
+        title: "Otaku".into(),
+        name: Some("otaku.app".into()),
+        transparent: true,
+        decorations: false,
+        skip_taskbar: true,
+        window_level: WindowLevel::AlwaysOnTop,
+        resolution: (300., 300.).into(),
+        ..default()
+    };
+
+    App::new()
+        .add_plugins((DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(window),
+            ..default()
+        }),))
+        .insert_resource(ClearColor(Color::NONE))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
