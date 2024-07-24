e9a9385a6e8c28b077a984d9b054b123573b46f4
Waradu <waradu@outlook.com>
2024-07-24 20:41:31 +0200

    added sprite

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use bevy::{prelude::*, window::WindowLevel};
 
+#[derive(Resource)]
+struct EventTimer(Timer);
+
 fn main() {
     let window = Window {
         title: "Otaku".into(),
@@ -17,11 +20,23 @@ fn main() {
             primary_window: Some(window),
             ..default()
         }),))
+        .insert_resource(EventTimer(Timer::from_seconds(10., TimerMode::Repeating)))
         .insert_resource(ClearColor(Color::NONE))
         .add_systems(Startup, setup)
         .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
+
+    let texture = asset_server.load("./pet/idle/default/normal/1/000.png");
+
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            custom_size: Some(Vec2::new(300., 300.)),
+            ..default()
+        },
+        texture,
+        ..default()
+    });
 }
